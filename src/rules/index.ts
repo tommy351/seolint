793bf23405e08f2ef585bea730a88f71faf5ab4f
Tommy Chen <tommy351@gmail.com>
2018-04-15 00:44:37 +0800

    Implement only-one-h1 rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,4 +1,5 @@
 export default {
   'a-rel': require('./a-rel'),
-  'img-alt': require('./img-alt')
+  'img-alt': require('./img-alt'),
+  'only-one-h1': require('./only-one-h1')
 };
---
+++ src/rules/only-one-h1.ts
@@ -0,0 +1,18 @@
+import { LintError, Rule } from '../types';
+
+const rule: Rule = {
+  name: 'only-one-h1',
+  lint($) {
+    const errors: LintError[] = [];
+
+    if ($('h1').length > 1) {
+      errors.push({
+        message: 'More than one <h1> tags exist'
+      });
+    }
+
+    return { errors };
+  }
+};
+
+export = rule;
