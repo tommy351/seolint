fc740be3540d7d7866151314cb93de9760b4df6f
Tommy Chen <tommy351@gmail.com>
2018-04-15 16:12:42 +0800

    Add tests for max-strong rule

---
+++ src/rules/__tests__/max-strong.ts
@@ -0,0 +1,44 @@
+import { load } from 'cheerio';
+import { LintResult } from '../../types';
+import rule from '../max-strong';
+
+let input: string;
+let $: CheerioStatic;
+let result: LintResult;
+
+function fakeInput(size: number) {
+  let s = '<body>';
+
+  for (let i = 0; i < size; i++) {
+    s += `<strong>${i}</strong>`;
+  }
+
+  return s + '</body>';
+}
+
+beforeEach(() => ($ = load(input)));
+beforeEach(() => (result = rule.lint($, 2)));
+
+describe('when the number of <strong> tag is larger than the maximum', () => {
+  beforeAll(() => (input = fakeInput(3)));
+
+  it('should have an error', () => expect(result.errors).toHaveLength(1));
+
+  it('check message', () =>
+    expect(result.errors[0]).toHaveProperty(
+      'message',
+      'The number of <strong> tag must not larger than 2'
+    ));
+});
+
+describe('when the number of <strong> tag equals to the maximum', () => {
+  beforeAll(() => (input = fakeInput(2)));
+
+  it('should have an error', () => expect(result.errors).toHaveLength(0));
+});
+
+describe('when no <strong> tag exists', () => {
+  beforeAll(() => (input = fakeInput(0)));
+
+  it('should have an error', () => expect(result.errors).toHaveLength(0));
+});
